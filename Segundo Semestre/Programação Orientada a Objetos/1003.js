714c9a108f14fbc0aaecf08da4f55b31a43bf935
Aluisio Pereira <oaluiser@gmail.com>
2023-08-13 19:50:48 -0300

    aula1

---
+++ Segundo Semestre/Programação Orientada a Objetos/1003.js
@@ -0,0 +1,8 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+console.log(lines)
+
+let a = parseInt(lines[0])
+let b = parseInt(lines[1])
+
+console.log("SOMA = " + (a + b))
---
+++ Segundo Semestre/Programação Orientada a Objetos/1114.js
@@ -0,0 +1,15 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+let i = 0
+let pass = "";
+
+while (pass != 2002) {
+    pass = lines[i]
+    if (pass == 2002) {
+        console.log("Acesso Permitido")
+    } else {
+        console.log("Senha Invalida")
+    }
+    i++
+}
